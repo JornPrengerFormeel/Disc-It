02df3a074e843be1be164552ea6e052e24f9f15b
Samalot <samalotplays@gmail.com>
2019-02-27 13:59:48 +0000

    spelling

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,8 +2,7 @@ const SpotifyWebApi = require('spotify-web-api-node'),
       spotifyApi = new SpotifyWebApi( require('../../credentials.json') ),
       crypto = require('crypto');
 
-
-// User links their Spotify account to the app
+// Generates a URL the users visits to link their account.
 exports.get_login_url = function(req, res) {
   let scopes = ["user-top-read", "playlist-modify-private", "user-follow-read", "user-library-read"],
       state = crypto.randomBytes(4).toString('hex'),
