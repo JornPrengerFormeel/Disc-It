06ed782102ad7f30999dad201d99d4878f7fe185
Samalot <samalotplays@gmail.com>
2019-02-27 14:14:20 +0000

    fixed folder structure

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 3000,
+      bodyParser = require('body-parser'),
+      cors = require('cors');
+
+app.use( cors({credentials: true, origin: true}) );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Register the different routes
+require('./api/routes')(app);
+
+// Register the fallback error message
+app.use(function(req, res) {
+  res.status(404).send( {message: req.originalUrl + ' not found'} )
+});
+
+// Start the server
+app.listen(port);
+console.log('Listening on port: ' + port);
