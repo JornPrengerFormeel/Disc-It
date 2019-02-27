0b052c4755c084da242753c6790cfcce570c32fb
Samalot <samalotplays@gmail.com>
2019-02-27 15:17:17 +0000

    changed method to GET

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,5 +1,5 @@
 const user_controller = require('../controllers/user');
 
 module.exports = (app) => {
-  app.route('/user/login').post(user_controller.get_login_url);
+  app.route('/user/login').get(user_controller.get_login_url);
 };
