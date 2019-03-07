a68efb875afac39cab39887d1b81e3d1c32bfe06
Guillaume Pitot <gpitot@nine.com.au>
2019-03-07 16:31:49 +1100

    returns correct data type based on response

--- src/api/BaseRequest.js
+++ src/api/BaseRequest.js
@@ -1,41 +1,49 @@
 
 class BaseRequest {
-  static async sendGetRequest(urlString) {
-    return new Promise((resolve, reject) => {
-      fetch(urlString,
-        {
-          method: 'get',
-          headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-          }
+    static async sendGetRequest(urlString, headers=null) {
+        if (headers === null) {
+            headers = {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+            }
         }
-      )
-        .then((response) => {
-          resolve(response.text());
-        })
-        .catch(reject);
-    });
-  }
+        try {
+            const data = await fetch(urlString, {
+                method: 'get',
+                headers: headers
+            });
+            const responseType = data.headers.get('content-type');
+            console.log(responseType);
+            if (responseType.includes('application/json')) {
+                const json = await data.json();
+                return json;
+            }
 
-
-
-  static async sendPostRequest(urlString, body) {
-    try {
-      const data = await fetch(urlString, {
-        method : 'post',
-        headers : {
-          'Content-Type':'application/json',
-        },
-        body : JSON.stringify(body)
-      });
-
-      const json = await data.json();
-      return json;
-    } catch (err) {
-      return {err:err};
+            return data.text();
+            
+        } catch (err) {
+            return { err };
+        }
+       
     }
-  }
+
+
+    static async sendPostRequest(urlString, body) {
+        try {
+            const data = await fetch(urlString, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            });
+
+            const json = await data.json();
+            return json;
+        } catch (err) {
+            return { err };
+        }
+    }
 }
 
-export {BaseRequest};
+export { BaseRequest };
