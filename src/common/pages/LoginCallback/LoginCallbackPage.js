01f025179b386c7890b078b8278a13d8a94cdc81
Guillaume Pitot <gpitot@nine.com.au>
2019-03-07 16:33:13 +1100

    puts tokens in local storage, redirects to discover

--- src/common/pages/LoginCallback/LoginCallbackPage.js
+++ src/common/pages/LoginCallback/LoginCallbackPage.js
@@ -1,42 +1,37 @@
 import React from 'react';
 
-import {DiscItApi} from '../../../api/discit-api/DiscItApi'
+import { DiscItApi } from '../../../api/discit-api/DiscItApi';
 
 class LoginCallbackPage extends React.Component {
-
-  state = {
-    tokens : null
-  }
+ 
 
   componentDidMount() {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get('code');
-    
-    
-    this.getTokens(code);
+      const params = new URLSearchParams(window.location.search);
+      const code = params.get('code');
+
+
+      this.getTokens(code);
   }
 
   async getTokens(code) {
-    const data = await DiscItApi.getTokens(code);
-    console.log(data);
-    this.setState({tokens:data})
+      const data = await DiscItApi.getTokens(code);
+      /* handle error here */
+      
+      if (data.access_token) {
+          this.setLocalSession(data);
+      }
+  }
+
+  setLocalSession(data) {
+      localStorage.setItem('access_token', data.access_token);
+      localStorage.setItem('refresh_token', data.refresh_token);
+      /* later consider adding epoch time of expiry so we can refresh before making a request */
+      window.location = '/discover';
   }
 
   render() {
-    if (this.state.tokens === null) return "Loading..."
-    
-    const tokens = this.state.tokens;
-    return (
-      <div>
-        {tokens.error && `Error - ${tokens.error_description}`}
-
-        {tokens.access_token && `Access Code - ${tokens.access_token}`}
-        <p></p>
-        {tokens.refresh_token && `Refresh Code - ${tokens.refresh_token}`}
-      </div>
-    )
+      return 'Loading...';
   }
 }
 
 export default LoginCallbackPage;
-
